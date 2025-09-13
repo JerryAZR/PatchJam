6fa159d24e68a8d9ea192404e3085cd57a35e329
phoenixming <phoenixzym139@gmail.com>
2025-09-13 23:06:54 +0800

    fix

--- Assets/Scripts/UI/BoxUI.cs
+++ Assets/Scripts/UI/BoxUI.cs
@@ -103,7 +103,7 @@ public class BoxUI : MonoBehaviour
         // }
         if (_countText != null)
             UpdateCount(); 
-        if (_weight != null)
+        if (_weightText != null)
             UpdateWeight();
     }
 
