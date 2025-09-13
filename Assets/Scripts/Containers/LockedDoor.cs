7ac6da81d73ba99476d8889470963f3bd7e79689
Jerry An <zerui.an@outlook.com>
2025-09-13 18:30:54 +0800

    Create TestLevel0 and import TMP
    Signed-off-by: Jerry An <zerui.an@outlook.com>

---
+++ Assets/Scripts/Containers/LockedDoor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LockedDoor : ContainerBase
+{
+    public bool Open { get; private set; } = false;
+    [SerializeField] private ItemBaseSO _key;
+    [SerializeField] private Animator _unlockAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Open) return;
+        foreach (ItemBaseSO item in _slots)
+        {
+            if (item == _key)
+            {
+                Open = true;
+                _unlockAnimator.SetTrigger("Unlock");
+            }
+
+        }
+    }
+}
