26173052f64cff7982ee174aa29f78cc6f89e0e4
Jerry An <zerui.an@outlook.com>
2025-09-14 21:35:10 +0800

    Add new level 2; Fix level selector room
    Signed-off-by: Jerry An <zerui.an@outlook.com>

--- Assets/Scripts/Level/ProgressManager.cs
+++ Assets/Scripts/Level/ProgressManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using MoreMountains.Tools;
 using UnityEngine;
 
@@ -10,10 +11,12 @@ public enum LevelState
     Cleared
 }
 
-public class ProgressManager : MMSingleton<ProgressManager>
+public class ProgressManager : MMPersistentSingleton<ProgressManager>
 {
     Dictionary<string, LevelState> _levelStates = new();
-    [SerializeField] private string[] _allLevels;
+    Dictionary<string, int> _levelOrder;
+    [SerializeField] private GameObject _gatesRoot;
+    private LevelEntrance[] _entranceArray;
 
     public LevelState GetState(string levelName) =>
         _levelStates.GetValueOrDefault(levelName, LevelState.Locked);
@@ -22,13 +25,24 @@ public class ProgressManager : MMSingleton<ProgressManager>
     {
         // Set this level as cleared and unlock next level
         _levelStates[levelName] = LevelState.Cleared;
+        int next = _levelOrder[levelName] + 1;
+        if (GetState(GetLevelName(next)) == LevelState.Locked)
+        {
+            _levelStates[GetLevelName(next)] = LevelState.Ready;
+        }
     }
 
     private void Start()
     {
-        if (_allLevels.Length > 0 && GetState(_allLevels[0]) == LevelState.Locked)
+        if (_entranceArray != null || _gatesRoot == null) return;
+        _entranceArray = _gatesRoot.GetComponentsInChildren<LevelEntrance>();
+        _levelOrder = Enumerable.Range(0, _entranceArray.Length)
+            .ToDictionary(i => GetLevelName(i), i => i);
+        if (GetState("Level1") == LevelState.Locked)
         {
-            _levelStates[_allLevels[0]] = LevelState.Ready;
+            _levelStates["Level1"] = LevelState.Ready;
         }
     }
+
+    private string GetLevelName(int index) => $"Level{index + 1}";
 }
