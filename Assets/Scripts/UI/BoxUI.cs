e6e546fa8d4ec6b6b01af6d73454605c89fb0af3
phoenixming <phoenixzym139@gmail.com>
2025-09-14 14:09:52 +0800

    fix

--- Assets/Scripts/UI/BoxUI.cs
+++ Assets/Scripts/UI/BoxUI.cs
@@ -153,7 +153,7 @@ public class BoxUI : MonoBehaviour
         PlayerBag.Instance.DebugPrint();
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
